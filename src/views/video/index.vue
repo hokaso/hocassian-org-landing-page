d24760d028b69465c729f2dab52a4eb3d13420f4
hokaso <hokaso@qq.com>
2022-04-10 11:15:10 +0800

    update：升级依赖包

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -3,7 +3,7 @@
     <div class="menu">
       <a class="btn" href="https://video.hocassian.com" target="_blank">同和視頻矩陣</a>
       <a class="btn" href="https://material.hocassian.com" target="_blank">同和素材矩陣</a>
-      <a class="btn" href="https://bbs.hocassian.com" target="_blank">同和創作矩陣</a>
+      <a class="btn" href="https://create.hocassian.com" target="_blank">同和創作矩陣</a>
     </div>
     <div class="banner">
       <video
@@ -14,70 +14,69 @@
         <p>NMCN·小眾多頻道網絡</p>
       </div>
     </div>
-    <img src="./shadow.png" class="shadow" alt=""/>
   </div>
 </template>
 
 <style lang="less" scoped>
-.root {
-  .menu {
-    position: absolute;
-    right: 3%;
-    top: 36px;
-    z-index: 1000000;
-    background-color: #0000003b;
-    padding: 10px 40px 10px 0;
-    border-radius: 10px;
-    .btn {
-      border-bottom: 2px solid #fff;
-      display: inline-block;
-      color: white;
-      font-weight: 600;
-      font-size: 15px;
-      position: relative;
-      margin-left: 44px;
-      line-height: 28px;
-      -webkit-font-smoothing: antialiased;
-      font-smoothing: antialiased;
-      cursor: pointer;
-      text-decoration: none;
-    }
-  }
-  .banner {
-    position: relative;
-    width: 100%;
-    height: 100vh;
-    .banner-content {
+  .root {
+    .menu {
       position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 1;
-      text-align: center;
-      & > h1 {
-        font-size: 40px;
-        font-weight: 500;
-        color: #ffffff;
-        text-shadow: 1px 3px 4px rgba(0, 0, 0, 0.64);
-        letter-spacing: 16px;
+      right: 3%;
+      top: 36px;
+      z-index: 1000000;
+      background-color: #0000003b;
+      padding: 10px 40px 10px 0;
+      border-radius: 10px;
+      .btn {
+        border-bottom: 2px solid #fff;
+        display: inline-block;
+        color: white;
+        font-weight: 600;
+        font-size: 15px;
+        position: relative;
+        margin-left: 44px;
+        line-height: 28px;
+        -webkit-font-smoothing: antialiased;
+        font-smoothing: antialiased;
+        cursor: pointer;
+        text-decoration: none;
       }
-      & > p {
-        text-shadow: 1px 3px 4px rgba(0, 0, 0, 0.64);
-        font-size: 20px;
-        font-weight: 500;
-        color: rgba(255, 255, 255, 0.75);
-        margin: 4px 0 28px;
-        letter-spacing: 8px;
+    }
+    .banner {
+      position: relative;
+      width: 100%;
+      .banner-content {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1;
+        text-align: center;
+        & > h1 {
+          font-size: 40px;
+          font-weight: 500;
+          color: #ffffff;
+          text-shadow: 1px 3px 4px rgba(0, 0, 0, 0.64);
+          letter-spacing: 16px;
+        }
+        & > p {
+          text-shadow: 1px 3px 4px rgba(0, 0, 0, 0.64);
+          font-size: 20px;
+          font-weight: 500;
+          color: rgba(255, 255, 255, 0.75);
+          margin: 4px 0 28px;
+          letter-spacing: 8px;
+        }
+      }
+      &::after{
+        content: '';
+        height: 100%;
+        width: 100%;
+        background: url(./shadow.png);
+        position: absolute;
+        left: 0;
+        top: 0;
       }
     }
   }
-  .shadow {
-    position: absolute;
-    width: 100%;
-    height: inherit;
-    z-index: 1;
-    top: 0;
-    left: 0;
-  }
-}
 </style>
