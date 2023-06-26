5a140b9a2471f261b4edc1d01658b6d4ddfb92a5
Hocassian <hokaso@qq.com>
2023-06-26 11:33:37 +0800

    update: 来自承翰的微调

--- src/components/top-menu/data.ts
+++ src/components/top-menu/data.ts
@@ -12,18 +12,18 @@ export const menuData = [
         ],
     },
     {
-        title: '开源共建',
+        title: '開源共建',
         key: '2',
         iconClass: 'fa-solid fa-circle',
         children: [
             {
-                title: '项目仓库',
+                title: '項目倉庫',
                 key: '2-1',
                 children: [
-                    {title: '同和新媒体矩阵', key: '2-1-1', href: 'https://github.com/hokaso/hocassian-media-matrix'},
-                    {title: '可视化人脉图谱', key: '2-1-2', href: 'https://github.com/hokaso/hocassian-people-neo4j'},
-                    {title: '矩阵官网', key: '2-1-3', href: 'https://github.com/hokaso/hocassian-org-landing-page'},
-                    {title: '视频处理小工具', key: '2-1-4', href: 'https://github.com/hokaso/auto_clip/releases/tag/auto_cut-hks-2022.11.07'},
+                    {title: '同和新媒體矩陣', key: '2-1-1', href: 'https://github.com/hokaso/hocassian-media-matrix'},
+                    {title: '可視化人脈圖譜', key: '2-1-2', href: 'https://github.com/hokaso/hocassian-people-neo4j'},
+                    {title: '矩陣官網', key: '2-1-3', href: 'https://github.com/hokaso/hocassian-org-landing-page'},
+                    {title: '視頻處理小工具', key: '2-1-4', href: 'https://github.com/hokaso/auto_clip/releases/tag/auto_cut-hks-2022.11.07'},
                 ],
             },
             {
@@ -34,15 +34,15 @@ export const menuData = [
         ],
     },
     {
-        title: '下载',
+        title: '下載',
         iconClass: 'fa-solid fa-circle',
         key: '3',
         children: [
-            {title: '同和智创工具箱', key: '3-1', href: '#software-download'},
-            {title: 'AIGC生产流水线', key: '3-2', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/技术赋能创作：如何通过AIGC搭建新媒体信息流生产流水线.pdf'},
-            {title: '同和新媒体矩阵·详细介绍', key: '3-3', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同和新媒体矩阵·详细介绍.pdf'},
-            {title: '同合杉天·公司简介', key: '3-4', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同合杉天·智能创作能力介绍手册.pdf'},
-            {title: '同合杉天·商业计划书', key: '3-5', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同合杉天·商业计划书.pdf'},
+            {title: '同和智創工具箱', key: '3-1', href: '#software-download'},
+            {title: 'AIGC生產流水綫', key: '3-2', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/技术赋能创作：如何通过AIGC搭建新媒体信息流生产流水线.pdf'},
+            {title: '同和新媒體矩陣·詳細介紹', key: '3-3', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同和新媒体矩阵·详细介绍.pdf'},
+            {title: '同合杉天·公司介紹', key: '3-4', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同合杉天·智能创作能力介绍手册.pdf'},
+            {title: '同合杉天·商業計劃書', key: '3-5', href: 'https://ops.hocassian.com:9000/prod-api/profile/video_matrix/landing_page/同合杉天·商业计划书.pdf'},
         ],
     },
 ];
